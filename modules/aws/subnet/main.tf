a343cc31ccc97b07034090fa40786cb62c7c06c2
muncha <koopatroopa2demacia@gmail.com>
2026-06-12 23:47:27 +0900

    import subnet

--- modules/aws/subnet/main.tf
+++ modules/aws/subnet/main.tf
@@ -1,7 +1,33 @@
 resource "aws_subnet" "public_subnet_1c" {
-  cidr_block = "10.0.64.0/18"
+  cidr_block              = "10.0.64.0/18"
+  map_public_ip_on_launch = true
   tags = {
     Name = "public-subnet-1c-${var.env}"
+  }
+  vpc_id = var.vpc_id
+}
+
+resource "aws_subnet" "public_subnet_1a" {
+  cidr_block              = "10.0.0.0/18"
+  map_public_ip_on_launch = true
+  tags = {
+    Name = "public-subnet-1a-${var.env}"
+  }
+  vpc_id = var.vpc_id
+}
+
+resource "aws_subnet" "private_subnet_1a" {
+  cidr_block = "10.0.128.0/18"
+  tags = {
+    Name = "private-subnet-1a-${var.env}"
+  }
+  vpc_id = var.vpc_id
+}
+
+resource "aws_subnet" "private_subnet_1c" {
+  cidr_block = "10.0.192.0/18"
+  tags = {
+    Name = "private-subnet-1c-${var.env}"
   }
   vpc_id = var.vpc_id
 }
