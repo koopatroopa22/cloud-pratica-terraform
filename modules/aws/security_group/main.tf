a3d4cb929e5693bfc54726830bb6099fae873982
muncha <koopatroopa2demacia@gmail.com>
2026-06-13 14:58:54 +0900

    rename sg_alb

--- modules/aws/security_group/main.tf
+++ modules/aws/security_group/main.tf
@@ -35,8 +35,10 @@ resource "aws_vpc_security_group_egress_rule" "cp_alb" {
 }
 
 resource "aws_vpc_security_group_ingress_rule" "cp_alb" {
+  for_each = toset(["0.0.0.0/0"])
+
   security_group_id = aws_security_group.cp-alb.id
-  cidr_ipv4         = "0.0.0.0/0"
+  cidr_ipv4         = each.value
   from_port         = 443
   to_port           = 443
   ip_protocol       = "tcp"
