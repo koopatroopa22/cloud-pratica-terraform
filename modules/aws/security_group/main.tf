e4f112317973a7ec4171ea0d7ebfa92a7f584941
muncha <koopatroopa2demacia@gmail.com>
2026-06-13 14:38:47 +0900

    import sg-alb

--- modules/aws/security_group/main.tf
+++ modules/aws/security_group/main.tf
@@ -21,3 +21,23 @@ resource "aws_vpc_security_group_egress_rule" "cp_bastion" {
   cidr_ipv4         = "0.0.0.0/0"
   ip_protocol       = "-1"
 }
+
+resource "aws_security_group" "cp-alb" {
+  name        = "cp-alb-${var.env}"
+  description = "alb sg"
+  vpc_id      = var.vpc_id
+}
+
+resource "aws_vpc_security_group_egress_rule" "cp_alb" {
+  security_group_id = aws_security_group.cp-alb.id
+  cidr_ipv4         = "0.0.0.0/0"
+  ip_protocol       = "-1"
+}
+
+resource "aws_vpc_security_group_ingress_rule" "cp_alb" {
+  security_group_id = aws_security_group.cp-alb.id
+  cidr_ipv4         = "0.0.0.0/0"
+  from_port         = 443
+  to_port           = 443
+  ip_protocol       = "tcp"
+}
