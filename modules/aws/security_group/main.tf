56df3a56e2fc008462823b4c30db4e98593fc37b
muncha <koopatroopa2demacia@gmail.com>
2026-06-13 12:58:19 +0900

    import sg bastion

--- modules/aws/security_group/main.tf
+++ modules/aws/security_group/main.tf
@@ -4,8 +4,20 @@ resource "aws_security_group" "db-migrator" {
   vpc_id      = var.vpc_id
 }
 
-resource "aws_vpc_security_group_egress_rule" "db-migrator-egress" {
+resource "aws_vpc_security_group_egress_rule" "db_migrator" {
   security_group_id = aws_security_group.db-migrator.id
+  cidr_ipv4         = "0.0.0.0/0"
+  ip_protocol       = "-1"
+}
+
+resource "aws_security_group" "cp-bastion" {
+  name        = "cp-bastion-${var.env}"
+  description = "bastion sg"
+  vpc_id      = var.vpc_id
+}
+
+resource "aws_vpc_security_group_egress_rule" "cp_bastion" {
+  security_group_id = aws_security_group.cp-bastion.id
   cidr_ipv4         = "0.0.0.0/0"
   ip_protocol       = "-1"
 }
